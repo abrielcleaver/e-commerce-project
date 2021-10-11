287565aadc82bd030c79daff7cd02e12c500c961
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-11 14:36:06 -0700

    add basic HTML, styles, and data model

---
+++ products.js
@@ -0,0 +1,11 @@
+export const products = [
+    {
+        id: '1',
+        name: 'oil',
+        img: 'assets/mathilde-langevin-Pv7SK92At0o-unsplash.jpg',
+        description: 'skin oil',
+        type: 'skincare',
+        price: '10',
+
+    }
+]
