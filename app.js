cc5f0b90d770b99e9add23165fb862b443ae8031
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-11 21:38:53 -0700

    fixing indentations

--- app.js
+++ app.js
@@ -7,12 +7,12 @@
   // use user input to update state 
   // update DOM to reflect the new state
 
-  import { renderProduct } from './render-product.js';
-  import { products } from './products.js';
+import { renderProduct } from './render-product.js';
+import { products } from './products.js';
 
-  const productList = document.getElementById('product-list');
+const productList = document.getElementById('product-list');
 
-  for (let product of products ){
-    const productCard = renderProduct(product);
-    productList.append(productCard);
-  }
+for (let product of products ){
+  const productCard = renderProduct(product);
+  productList.append(productCard);
+}
