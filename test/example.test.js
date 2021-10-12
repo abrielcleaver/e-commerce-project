63f493c7e31522b6d32378916d9d7416b6a95d3f
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-12 14:47:22 -0700

    renderProduct test passed

--- test/example.test.js
+++ test/example.test.js
@@ -9,7 +9,7 @@ const test = QUnit.test;
 test('renderProduct should return HTML snippet', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = `<div class="product-card"><h2>Pothos</h2><img src="./assets/pothos-severin-candrian-GG9Gh1_FjbM-unsplash.jpg"><p>Epipremnum aureum, also known as pothos, is native to Mo'orea</p><p>Tropical</p><p>$20</p><button value=\"1\">ADD TO CART</button></div>`;
+    const expected = `<div class="product-card"><h2>Pothos</h2><img src="./assets/pothos-severin-candrian-GG9Gh1_FjbM-unsplash.jpg"><p>Epipremnum aureum, also known as pothos, is native to Mo'orea</p><p>Tropical</p><p>20</p><button value=\"1\">ADD TO CART</button></div>`;
     const pothos = products[0];
     //Act 
     // Call the function you're testing and set the result to a const
