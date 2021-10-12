dba516e9689c25a9477f53795a29997dfafa468f
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-12 14:15:57 -0700

    render function test passed

--- data/products.js
+++ data/products.js
@@ -2,7 +2,7 @@ export const products = [
     {
         id: '1',
         name: 'Pothos',
-        img: 'assets/pothos-severin-candrian-GG9Gh1_FjbM-unsplash.jpg',
+        img: './assets/pothos-severin-candrian-GG9Gh1_FjbM-unsplash.jpg',
         description: 'Epipremnum aureum, also known as pothos, is native to Mo\'orea',
         type: 'Tropical',
         price: '$20'
@@ -11,7 +11,7 @@ export const products = [
     {
         id: '2',
         name: 'Aloe Vera',
-        img: 'assets/aloe-severin-candrian-nU9U66FMutc-unsplash.jpg',
+        img: './assets/aloe-severin-candrian-nU9U66FMutc-unsplash.jpg',
         description: 'Aloe vera is a succulent plant species of the genus Aloe.',
         type: 'Succulent',
         price: 15
@@ -19,7 +19,7 @@ export const products = [
     {
         id: '3',
         name: 'Monstera Deliciosa',
-        img: 'assets/monstera-severin-candrian-bwsTJMnhcwE-unsplash.jpg',
+        img: './assets/monstera-severin-candrian-bwsTJMnhcwE-unsplash.jpg',
         description: 'Monstera deliciosa, also known as the "split-leaf philodendron, is native to tropical forests of southern Mexico.',
         type: 'Tropical',
         price: 45
@@ -35,7 +35,7 @@ export const products = [
     {
         id: '5',
         name: 'Aglaonema Cocomelon',
-        img: 'assets/Aglaonema Cocomelon-severin-candrian-Qhpuw3lNVpQ-unsplash.jpg',
+        img: './assets/Aglaonema Cocomelon-severin-candrian-Qhpuw3lNVpQ-unsplash.jpg',
         description: 'Aglaonema grow wild in the tropical rain forests of South East Asia.',
         type: 'Tropical',
         price: 25
@@ -43,7 +43,7 @@ export const products = [
     {
         id: '6',
         name: 'Sansevieria Kirkii',
-        img: 'assets/sansevieriakirkii-severin-candrian-yZIjR21cgZ0-unsplash.jpg',
+        img: './assets/sansevieriakirkii-severin-candrian-yZIjR21cgZ0-unsplash.jpg',
         description: 'Sansevieria kirkii, also known as Star Sansevieria.',
         type: 'Tropical',
         price: 25
--- test/example.test.js
+++ test/example.test.js
@@ -1,16 +1,18 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderProduct } from "../data/render-product.js";
+import { products} from "../data/products.js";
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('renderProduct should return HTML snippet', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
-    
+    const expected = `<div class="product-card"><h2>Pothos</h2><img src="./assets/pothos-severin-candrian-GG9Gh1_FjbM-unsplash.jpg"><p>Epipremnum aureum, also known as pothos, is native to Mo'orea</p><p>Tropical</p><p>$20</p><button value=\"1\">ADD TO CART</button></div>`;
+    const pothos = products[0];
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderProduct(pothos).outerHTML;
 
     //Expect
     // Make assertions about what is expected versus the actual result
