e304e5c1d35c36c269db8d57cb940cc4a724bbd0
Abriel Cleaver <abrielcleaver@gmail.com>
2021-10-13 13:29:19 -0700

    update render function

---
+++ render-line-item.js
@@ -0,0 +1,23 @@
+import { toUSD } from './utils.js';
+
+export function renderLineItem (cartItems, productData){
+    console.log(productData);
+    const tr = document.createElement('tr');
+    
+    const tdName = document.createElement('td');
+    tdName.textContent = productData.name;
+    
+    const tdPrice = document.createElement('td');
+    tdPrice.textContent = productData.price;
+    
+    const tdQty = document.createElement('td');
+    tdQty.textContent = cartItems.qty;
+    
+    const tdTotal = document.createElement('td');
+    const total = cartItems.qty * productData.price;
+    tdTotal.textContent = toUSD(total);
+
+    tr.append(tdName, tdPrice, tdQty, tdTotal);
+
+    return tr;
+}
--- utils.js
+++ utils.js
@@ -8,3 +8,22 @@ export function findById(id, items){
         // else continue the loop
     }
 }
+
+export function calculateOrderTotal(cart, products) {
+    // initialize a variable to keep track of total
+    // loop through the cart items
+    // get the associated product from the product list
+    // add the qty * price to the total
+    // return the total
+    
+    let orderTotal = 0;
+    for (let item of cart){
+        const product = findById(item.id, products);
+        orderTotal = orderTotal + product.price * item.qty;
+    }
+    return orderTotal;
+}
+
+export function toUSD(number){
+    return number.toLocaleString('en-us', { style: 'currency', currency: 'USD' });
+}
